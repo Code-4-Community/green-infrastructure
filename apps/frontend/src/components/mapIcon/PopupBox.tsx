39c87d80d7a2e8ce8ec79713cae81e50376bc71c
David Li <dali7758769@gmail.com>
2023-11-15 22:00:50 -0500

    most

--- apps/frontend/src/components/mapIcon/PopupBox.tsx
+++ apps/frontend/src/components/mapIcon/PopupBox.tsx
@@ -1,22 +1,29 @@
 export function createPopupBoxContent(name: string, location: string, status: string, type: string, color: string, svgFunction: (color: string) => string) {
 
   return `
-  <head><link rel='stylesheet'/></head>
+  <head><link rel='stylesheet' href='../../styles.css'/>
+  <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Arimo"></head>
   <body>
     <div class='popup'>
       <div class='typeBox'>
         ${type ? 
-          `${svgFunction(color)}
-          <p class='featureType'>Feature Type: </p><p><b>${type.toUpperCase()}</b></p>` 
+          `<p class='featureType'> Feature Type: </p>
+           <p class='featureLabel'><b>${type.toUpperCase()}</b></p>
+          ${svgFunction(color)}` 
           : ''}
       </div>
       <div class='infoBox'>
-        ${name ? `<p><b>Name: </b>${name}</p>` : ''}
-        ${location ? `<p><b>Location: </b>${location}</p>` : ''}
-        ${status ? `<p><b>Status: </b>${status}</p>` : ''}
-        <a href="https://ma.adopt-a-drain.org/register?selectedDrainId=1084">Click here to adopt!</a>
+
+        ${status ? `<p class='statusText'><b>Status: </b>${status}</p>` : ''}
+        <a href="https://ma.adopt-a-drain.org/register?selectedDrainId=1084" class='adoptLink'>Interested in adopting →</a>
       </div>
     </div>
     </body>
     `;
 }
+
+/**
+ *       ${name ? `<p><b>Name: </b>${name}</p>` : ''}
+        ${location ? `<p><b>Location: </b>${location}</p>` : ''}
+ */
