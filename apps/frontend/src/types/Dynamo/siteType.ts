a00f89b29d648fef80ead4778689e751e7e3fd6b
jarenadams21 <jarenadams3@gmail.com>
2024-04-08 08:23:50 -0400

    site update

--- apps/frontend/src/types/Dynamo/siteType.ts
+++ apps/frontend/src/types/Dynamo/siteType.ts
@@ -1,17 +1,17 @@
-import SymbolType from "./assetType";
+import SymbolType from "./symbolType";
 
 export default interface Site {
     siteId: number;
     address: string;
     assetName: string;
     assetType: string;
-    latitude: number;
+    latitude: string;
+    longitude: string;
     linkToConstructionPlans: string;
     linkToFinalReports: string;
     linkToMaintenanceAgreement: string;
     linkToMaintenanceChecklist: string;
     linkToRFQ: string;
-    longitude: number;
     maintenanceAgreement: boolean;
     neighborhood: string;
     partnerDepartments: string; 
